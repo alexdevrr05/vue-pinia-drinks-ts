5c32865ea26bee9211c7a078bb3873471c0ecd4f
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-02-03 11:14:21 -0600

    Refactor CardRecipe component to improve styling and add recipe button

--- src/shared/components/CardRecipe.vue
+++ src/shared/components/CardRecipe.vue
@@ -9,7 +9,25 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div class="">
-    <h2>{{ $props.recipe }}</h2>
+  <div class="border shadow-lg">
+    <!-- Para que la imagen no sobresalga del card -->
+    <div class="overflow-hidden">
+      <img
+        class="hover:scale-110 transition-transform hover:rotate-2"
+        :src="$props.recipe?.strDrinkThumb"
+        :alt="`Imagen de Bebida ${$props.recipe?.strDrink}`"
+      />
+    </div>
+    <div class="p-5">
+      <h2 class="text-2xl truncate font-extrabold">
+        {{ $props.recipe?.strDrink }}
+      </h2>
+      <button
+        type="button"
+        class="bg-orange-400 hover:bg-orange-500 transition-colors mt-5 w-full p-3 font-bold text-white text-lg"
+      >
+        Ver receta
+      </button>
+    </div>
   </div>
 </template>
