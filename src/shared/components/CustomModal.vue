ee9898d35904e8b9dea9f989fca65e96323b9db0
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-02-03 20:26:35 -0600

    Update CustomModal.vue styling and layout

--- src/shared/components/CustomModal.vue
+++ src/shared/components/CustomModal.vue
@@ -22,7 +22,7 @@ const ingredientsFormat = () => {
       const qty = (drinks.details as any)[`strMeasure${i}`];
 
       const ingredientAndQty = document.createElement('p');
-      ingredientAndQty.classList.add('text-lg');
+      ingredientAndQty.classList.add('text-lg', 'text-gray-500');
       ingredientAndQty.textContent = `${ingredient} - ${qty}`;
 
       ingredientsDiv.appendChild(ingredientAndQty);
@@ -64,35 +64,57 @@ const ingredientsFormat = () => {
             <DialogPanel
               class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
             >
-              <div class="mt-2">
-                <DialogTitle
-                  as="h3"
-                  class="text-4xl font-extrabold my-5 text-gray-900"
-                >
-                  {{ drinks.details.strDrink }}
-                </DialogTitle>
+              <div>
+                <div class="mt-3">
+                  <DialogTitle
+                    as="h3"
+                    class="text-3xl font-extrabold my-5 text-gray-900"
+                  >
+                    {{ drinks.details.strDrink }}
+                  </DialogTitle>
 
-                <img
-                  :src="drinks.details.strDrinkThumb"
-                  :alt="`Imagen de ${drinks.details.strDrink}`"
-                />
+                  <img
+                    :src="drinks.details.strDrinkThumb"
+                    :alt="`Imagen de ${drinks.details.strDrink}`"
+                    class="mx-auto w-90"
+                  />
 
-                <DialogPanel
-                  as="h3"
-                  class="text-gray-900 text-4xl font-extrabold my-5"
-                >
-                  Ingredientes y cantidades
-                </DialogPanel>
-                <div v-html="ingredientsFormat().outerHTML"></div>
+                  <DialogPanel
+                    as="h3"
+                    class="text-gray-900 text-2xl font-extrabold my-5"
+                  >
+                    Ingredientes y cantidades
+                  </DialogPanel>
+
+                  <div v-html="ingredientsFormat().outerHTML"></div>
+
+                  <DialogPanel
+                    as="h3"
+                    class="text-gray-900 text-2xl font-extrabold my-5"
+                  >
+                    Instrucciones
+                  </DialogPanel>
+
+                  <p class="text-lg text-gray-500">
+                    {{ drinks.details.strInstructions }}
+                  </p>
+                </div>
               </div>
 
-              <div class="mt-4">
+              <div class="flex mt-5 justify-between gap-3">
                 <button
                   type="button"
-                  class="w-full text-white uppercase bg-gray-600 hover:bg-gray-500 shadow rounded p-3"
+                  class="w-full text-white uppercase bg-gray-600 hover:bg-gray-500 shadow rounded p-2"
                   @click="modal.handleClickModal"
                 >
                   Cerrar
+                </button>
+
+                <button
+                  type="button"
+                  class="w-full text-white uppercase bg-orange-600 hover:bg-orange-500 shadow rounded p-2"
+                >
+                  Agregar a favoritos
                 </button>
               </div>
             </DialogPanel>
